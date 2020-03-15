4e53f55453c25f43ed4606c93f8c137f7a3a283a
Igor Epstein <igor.epstein@jetpatch.com>
2020-03-15 15:12:46 +0200

    Fix shortcuts

--- run_once_fff_kde.sh
+++ run_once_fff_kde.sh
@@ -5,6 +5,9 @@ echo Processing shortcuts
 kwriteconfig5 --file "$HOME/.config/kglobalshortcutsrc" --group "krunner.desktop" --key "RunClipboard" "none,none,Run command on clipboard contents"
 kwriteconfig5 --file "$HOME/.config/kglobalshortcutsrc" --group "krunner.desktop" --key "_launch" "none,none,KRunner"
 kwriteconfig5 --file "$HOME/.config/kglobalshortcutsrc" --group "plasmashell" --key "show dashboard" "none,none,Show Desktop" 
+kwriteconfig5 --file "$HOME/.config/kglobalshortcutsrc" --group "ksmserver" --key "Lock Session" "Meta+L\tScreensaver,Meta+L\tScreensaver,Lock Session" 
+# Workaround for kwriteconfig escaping \t as \\t. Without quotes, \t is escaped as only t.
+sed -i 's@\\\\t@\\t@g' $HOME/.config/kglobalshortcutsrc
 
 # The name 'kglobalaccel' should have '5' only in the second case
 kquitapp5 kglobalaccel && sleep 2s && kglobalaccel5 &
